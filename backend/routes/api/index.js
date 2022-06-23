b4f6b5896f4b12eb8ef811a5a34c0772556e94cf
Josh Salcido <joshuasalcido21@gmail.com>
2022-06-23 14:34:41 -0600

    login route complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,12 +3,15 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
