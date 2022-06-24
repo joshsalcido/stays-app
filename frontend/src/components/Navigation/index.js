358bdf1d04eb209af2f8d20872e262688cda5ea8
Josh Salcido <joshuasalcido21@gmail.com>
2022-06-24 01:32:13 -0600

    navbar min styling

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login" className="login">Log In</NavLink>
+        <NavLink to="/signup" className="signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <div className='navBar'>
+      <ul>
+        <li>
+            <NavLink exact to="/" className="home">Home</NavLink>
+            {isLoaded && sessionLinks}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
