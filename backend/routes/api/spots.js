ec6e3546b542c3f42f05ecb91aa63b1022fc83b0
Josh Salcido <joshuasalcido21@gmail.com>
2022-06-27 16:35:19 -0600

    created slice of store and backend route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,10 +4,12 @@ const { Spot } = require('../../db/models');
 
 const router = express.Router();
 
-router.get('/spots', asyncHandler(async(req, res) => {
-    const spots = await Spot.findAll({
-        include: ['images'],
-    });
-    console.log(res.json(spots));
+router.get('/', asyncHandler(async(req, res) => {
+    console.log('BEFORE FINDALL ******')
+    const spots = await Spot.findAll();
+    console.log(spots, "<----");
     return res.json(spots);
 }))
+
+
+module.exports = router;
