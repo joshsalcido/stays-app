2984d727b5ea83416162cd10b4fb0cbc83524a1f
Josh Salcido <joshuasalcido21@gmail.com>
2022-09-23 15:59:34 -0600

    added more styling to booking form

--- frontend/src/components/Booking/bookingForm.js
+++ frontend/src/components/Booking/bookingForm.js
@@ -5,8 +5,16 @@ export default function BookingForm({indSpot}){
         <>
         <form className='booking-form'>
             <h4 className="span-price">${parseInt(indSpot.price).toLocaleString("en-Us")} <span>night</span></h4>
-            <input type="date"></input>
-            <input type="date"></input>
+            <div className="checkin-out-div-container">
+                <div className="checkin-div">
+                    <label className="checkin-label">CHECK-IN</label>
+                    <input className="checkin-input" type="date" min="2022-09-23"></input>
+                </div>
+                <div className="checkout-div">
+                    <label className="checkout-label">CHECKOUT</label>
+                <input className="checkout-input" type="date"></input>
+                </div>
+            </div>
             <button>Reserve</button>
             <p>Cleaning Fee</p>
             <p>Service Fee</p>
