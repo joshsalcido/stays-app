5f29477d7b0a9497e8dfae6cc24a21698cdc97ab
Josh Salcido <joshuasalcido21@gmail.com>
2022-09-23 18:23:02 -0600

    created booking calendar with no errors allowed as inputs

--- frontend/src/components/Booking/bookingForm.js
+++ frontend/src/components/Booking/bookingForm.js
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import '../Booking/bookingform.css'
+import Moment from 'moment';
 
 export default function BookingForm({indSpot}){
+    const [checkOutOn, setCheckOutOn] = useState(true)
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut,  setCheckOut] = useState('')
+
+    const today = new Date()
+    const todayFormatted = Moment(today).format("YYYY-MM-DD")
+    const tomorrow = today.setDate(today.getDate() + 1);
+    const tomorrowFormatted = Moment(tomorrow).format("YYYY-MM-DD");
+    const checkInFormatted = Moment(checkIn).format("YYYY-MM-DD")
+    const checkInDate = new Date(checkInFormatted)
+    const checkOutMin = checkInDate.setDate(checkInDate.getDate() + 2)
+    const checkOutMinFormatted = Moment(checkOutMin).format("YYYY-MM-DD")
+
+
+    // console.log(checkOutMinFormatted, "CHECKIN VALUE")
+
     return (
         <>
         <form className='booking-form'>
@@ -8,11 +26,11 @@ export default function BookingForm({indSpot}){
             <div className="checkin-out-div-container">
                 <div className="checkin-div">
                     <label className="checkin-label">CHECK-IN</label>
-                    <input className="checkin-input" type="date" min="2022-09-23"></input>
+                    <input required className="checkin-input" type="date" min={todayFormatted} onChange={(e) => {setCheckOutOn(false); setCheckOut(''); setCheckIn(e.target.value)}}></input>
                 </div>
                 <div className="checkout-div">
                     <label className="checkout-label">CHECKOUT</label>
-                <input className="checkout-input" type="date"></input>
+                <input required className="checkout-input" type="date" min={checkOutMinFormatted} value={checkOut}  disabled={checkIn.length === 0} onChange={(e) => setCheckOut(e.target.value)}></input>
                 </div>
             </div>
             <button>Reserve</button>
